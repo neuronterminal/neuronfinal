1a332e906445de83f8e6b3d766e4a03853ac7b12
Jay <netneuron1@gmail.com>
2024-12-28 18:53:38 -0500

    Update d3.d.ts

--- src/types/d3.d.ts
+++ src/types/d3.d.ts
@@ -1,7 +1,20 @@
+import { SimulationNodeDatum, SimulationLinkDatum } from 'd3-force';
+
 declare module 'd3' {
   export * from 'd3-selection';
   export * from 'd3-force';
   export * from 'd3-scale';
   export * from 'd3-shape';
   export * from 'd3-array';
+
+  export interface Node extends SimulationNodeDatum {
+    id: string;
+    x?: number;
+    y?: number;
+  }
+
+  export interface Link extends SimulationLinkDatum<Node> {
+    source: string | Node;
+    target: string | Node;
+  }
 }
