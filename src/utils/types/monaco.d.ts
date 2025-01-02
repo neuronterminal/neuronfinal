acebe4bc5ec8aa9cab3e856e40876037b83e91e1
Jay <netneuron1@gmail.com>
2025-01-02 10:53:12 -0500

    Update monaco.d.ts

--- src/utils/types/monaco.d.ts
+++ src/utils/types/monaco.d.ts
@@ -1,3 +1,7 @@
+
 declare module '@monaco-editor/react' {
+  // Extending the IStandaloneThemeData interface
   interface IStandaloneThemeData {
-    base: 'vs' |
+    base: 'vs' | 'vs-dark' | 'hc-black'; // Correct theme options
+  }
+}
